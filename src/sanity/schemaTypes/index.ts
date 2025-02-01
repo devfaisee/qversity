2a0737a6f03411ac4155c08ba714dbfa7fecb420
devfaisee <160999052+devfaisee@users.noreply.github.com>
2025-02-01 13:16:09 +0500

    Update index.ts

---
+++ src/sanity/schemaTypes/index.ts
@@ -0,0 +1,8 @@
+import { type SchemaTypeDefinition } from 'sanity'
+
+export const schemaTypes = [product];
+
+// Register it
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: schemaTypes
+};
