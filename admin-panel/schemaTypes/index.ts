c6e1aeae55f3e40eaf7562e89debbea82d884eb9
devfaisee <dev.faisee@gmail.com>
2025-02-02 08:25:02 +0500

    deleted sanity

--- admin-panel/schemaTypes/index.ts
+++
@@ -1,8 +0,0 @@
-
-import { type SchemaTypeDefinition } from 'sanity'
-import product from "./product"; // Import product schema
-
-export const schemaTypes = [product]; // Register it
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
