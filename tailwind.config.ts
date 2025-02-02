211879cb108041b51e9d46a5875d64fb163102e9
devfaisee <160999052+devfaisee@users.noreply.github.com>
2025-02-02 14:00:17 +0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,40 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+
+
+.menu-item {
+  position: relative;
+  font-size: 1.1rem; /* Adjusts the font size for a sleek look */
+  letter-spacing: 1px; /* Adds a touch of spacing for elegance */
+  transition: all 0.3s ease; /* Smooth transition for hover effect */
+}
+
+.menu-item:before {
+  content: '';
+  position: absolute;
+  bottom: -5px;
+  left: 0;
+  width: 100%;
+  height: 2px;
+  background-color: #fff;
+  transform: scaleX(0);
+  transform-origin: bottom right;
+  transition: transform 0.25s ease-out;
+}
+
+.menu-item:hover:before {
+  transform: scaleX(1);
+  transform-origin: bottom left;
+}
+
+.menu-item:hover {
+  color: #FF63F5; /* Add a sexy hover effect with a soft pink color */
+  text-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2); /* Adds a subtle shadow for depth */
+}
+
+
+
       keyframes: {
         fadeIn: {
           '0%': { opacity: "0", transform: "translateY(-10px)" },
