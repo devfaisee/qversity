0204a0fdc72bc60f91562c10013769834e8c6d1d
devfaisee <160999052+devfaisee@users.noreply.github.com>
2025-02-09 11:53:46 +0500

    Update page.tsx

---
+++ src/app/shop/page.tsx
@@ -0,0 +1,9 @@
+export defualt function Shop = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-[#F8F9FA] text-[#212529]">
+      <h1 className="text-4xl font-bold">Welcome to the Shop</h1>
+    </div>
+  );
+};
+
+export default Shop;
