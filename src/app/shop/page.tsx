c9d29bd9624db04d934977ff1febc977e427da99
devfaisee <160999052+devfaisee@users.noreply.github.com>
2025-02-09 11:55:50 +0500

    Update page.tsx

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,4 +1,4 @@
-export defualt function Shop = () => {
+const Shop = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-[#F8F9FA] text-[#212529]">
       <h1 className="text-4xl font-bold">Welcome to the Shop</h1>
